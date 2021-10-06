c0d9cb3694fc1e7a564c6700ecd7a26eaaa3b3b6
panyurou <yrpan@jinshuju.net>
2021-10-06 14:10:04 +0800

    初始化Spring boot项目
    项目是在https://start.spring.io/创建的
    
    提前引入了依赖 Spring Web，HyperSQL Database， Lombok

---
+++ src/test/java/com/panyuro/learn_graphql/springbootgraphql_example/SpringBootGraphqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panyuro.learn_graphql.springbootgraphql_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphqlExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
